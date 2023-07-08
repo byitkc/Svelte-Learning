9692edecad4b6c45c689ea68ef13f0b033b20d8a
Brandon Young <brandon@byitkc.com>
2023-07-08 10:30:34 -0500

    Addition scss things

--- udemy01/src/lib/Button.svelte
+++ udemy01/src/lib/Button.svelte
@@ -21,6 +21,12 @@
         font-weight: bold;
         border-radius: 5px;
         cursor: pointer;
+        &:hover {
+            background-image: linear-gradient(rgba(0, 0, 0, 0.2) 0 0);
+        }
+        &:active {
+            background-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
+        }
         &.size-sm {
             padding: 15px 20px;
         }
