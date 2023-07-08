07eea2b8e72c56f4aeb646dbd486f8e8648ebfcc
Brandon Young <brandon@byitkc.com>
2023-07-08 11:44:56 -0500

    Binding input value to a Variable

--- udemy01/src/lib/TodoList.svelte
+++ udemy01/src/lib/TodoList.svelte
@@ -1,12 +1,26 @@
 <script>
+    import Button from "./Button.svelte";
+
     export let todos = [];
+    let inputText = "";
+
+    function handleAddTodo() {
+        console.log(inputText);
+    }
 </script>
 
-<ul>
-    {#each todos as { id, title }, index (id)}
-        <!-- We can define a variable inside of our loop, it can only be used as a child (ex. of a each or if) -->
-        {@const number = index + 1}
-        <!-- Then we can use that variable -->
-        <li>{number} - {title}</li>
-    {/each}
-</ul>
+<div class="todo-list-wrapper">
+    <ul>
+        {#each todos as { id, title }, index (id)}
+            <!-- We can define a variable inside of our loop, it can only be used as a child (ex. of a each or if) -->
+            {@const number = index + 1}
+            <!-- Then we can use that variable -->
+            <li>{number} - {title}</li>
+        {/each}
+    </ul>
+    {inputText}
+    <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+        <input bind:value={inputText} />
+        <Button type="submit">Add</Button>
+    </form>
+</div>
