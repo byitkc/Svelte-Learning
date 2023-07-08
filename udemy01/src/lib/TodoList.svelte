a6bbb590ae70de41e2913a9b6c784ed307cb53d0
Brandon Young <brandon@byitkc.com>
2023-07-08 11:51:26 -0500

    Updating Arrays and Objects

--- udemy01/src/lib/TodoList.svelte
+++ udemy01/src/lib/TodoList.svelte
@@ -1,11 +1,31 @@
 <script>
     import Button from "./Button.svelte";
+    import { v4 as uuid } from "uuid";
 
     export let todos = [];
     let inputText = "";
 
     function handleAddTodo() {
-        console.log(inputText);
+        if (!inputText) return;
+        // This is one way we can do this, but there is a better and cleaner way
+        // todos.push({
+        //     id: uuid(),
+        //     title: inputText,
+        //     completed: false,
+        // });
+        // todos = todos;
+
+        // Add the input text to the todos array
+        todos = [
+            ...todos,
+            {
+                id: uuid(),
+                title: inputText,
+                completed: false,
+            },
+        ];
+        // We can use this to clear the input after submission
+        inputText = "";
     }
 </script>
 
@@ -18,9 +38,8 @@
             <li>{number} - {title}</li>
         {/each}
     </ul>
-    {inputText}
     <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
         <input bind:value={inputText} />
-        <Button type="submit">Add</Button>
+        <Button type="submit" disabled={!inputText}>Add</Button>
     </form>
 </div>
