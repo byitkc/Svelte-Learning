1b72de83645a42937d143f8d6f0197624185c73b
Brandon Young <brandon@byitkc.com>
2023-07-08 10:06:31 -0500

    more usage of slot

--- udemy01/src/lib/Button.svelte
+++ udemy01/src/lib/Button.svelte
@@ -2,4 +2,17 @@
 </script>
 
 <!-- We can use "slot" to pull the text from the button defined between tags in App.svelte -->
-<button><slot /></button>
+<!-- If we use the slot tags and place something in them, it will be used if no slot is passed -->
+<button><slot>Fallback</slot></button>
+
+<style>
+	button {
+		border: none;
+		background-color: #ff3e00;
+		color: white;
+		padding: 15px 20px;
+		font-weight: bold;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+</style>
