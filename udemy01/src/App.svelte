c1c41af40531f3ca86e20301749458e268650bcb
Brandon Young <brandon@byitkc.com>
2023-07-08 10:48:52 -0500

    If statement for button and svg import

--- udemy01/src/App.svelte
+++ udemy01/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
+	import DiBootstrap from "svelte-icons/di/DiBootstrap.svelte";
 	import Button from "./lib/Button.svelte";
 </script>
 
 <!-- If we setup the button like this, we can use "slots" to pass the text in `./src/Button.svelte` -->
-<Button size="small" shadow>Button Text</Button>
+<Button size="small" shadow>
+	<div style:width="20px" slot="leftContent"><DiBootstrap /></div>
+	Button Text
+</Button>
 
 <style>
 </style>
--- udemy01/src/lib/Button.svelte
+++ udemy01/src/lib/Button.svelte
@@ -3,6 +3,8 @@
     export let shadow = false;
     export let bgColor = "inherit";
     export let textColor = "inherit";
+
+    console.log($$slots);
 </script>
 
 <!-- We can use "slot" to pull the text from the button defined between tags in App.svelte -->
@@ -13,11 +15,21 @@
     class:size-lg={size === "large"}
     class:size-md={size === "medium"}
     class:size-sm={size === "small"}
-    class:shadow><slot>Fallback</slot></button
+    class:shadow
+    class:has-left={$$slots.leftContent}
 >
+    {#if $$slots.leftContent}
+        <div class="left-content">
+            <slot name="leftContent" />
+        </div>
+    {/if}
+    <slot>Fallback</slot>
+</button>
 
 <style lang="scss">
     button {
+        display: flex;
+        align-items: center;
         border: none;
         background-color: var(--buttonBgColor);
         color: var(--buttonTextColor);
@@ -25,6 +37,9 @@
         font-weight: bold;
         border-radius: 5px;
         cursor: pointer;
+        .left-content {
+            margin-right: 10px;
+        }
         &:hover {
             background-image: linear-gradient(rgba(0, 0, 0, 0.2) 0 0);
         }
