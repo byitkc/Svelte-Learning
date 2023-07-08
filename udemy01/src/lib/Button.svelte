f25022dc6bd51bf3e2fc7ecf3c5ef2bc5a6a673c
Brandon Young <brandon@byitkc.com>
2023-07-08 10:22:14 -0500

    using scss and prependData

--- udemy01/src/lib/Button.svelte
+++ udemy01/src/lib/Button.svelte
@@ -5,14 +5,17 @@
 <!-- If we use the slot tags and place something in them, it will be used if no slot is passed -->
 <button><slot>Fallback</slot></button>
 
-<style>
-	button {
-		border: none;
-		background-color: #ff3e00;
-		color: white;
-		padding: 15px 20px;
-		font-weight: bold;
-		border-radius: 5px;
-		cursor: pointer;
-	}
+<style lang="scss">
+    button {
+        border: none;
+        background-color: #ff3e00;
+        color: white;
+        padding: 15px 20px;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+            background-color: variables.$color;
+        }
+    }
 </style>
