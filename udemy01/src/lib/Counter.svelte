d1e5c14ffaa582990d5bc3f5be4e1febcf64d7b7
Brandon Young <brandon@byitkc.com>
2023-07-08 09:50:00 -0500

    Order of statements matters

--- udemy01/src/lib/Counter.svelte
+++ udemy01/src/lib/Counter.svelte
@@ -3,28 +3,25 @@
 	let count1 = 0;
 	let count2 = 0;
 
+	function setCount2(x) {
+		count2 = x;
+	}
+
 	// Reactive statements run whenever the values they depend on change
 
 	// Not going to work
-	$: string = `The count total is ${getTotal()}`;
+	$: setCount2(count1);
+	// Order of statements matters, so this will work, but if this statement is above the previous one, it won't work
+	$: string = `Count2 is ${count2}`;
 
 	// End Reactive statements
-
-	function getTotal() {
-		return count1 + count2;
-	}
 
 	function increment1() {
 		count1 += 1;
 	}
-
-	function increment2() {
-		count2 += 1;
-	}
 </script>
 
 <button on:click={increment1}>Clicks: {count1}</button>
-<button on:click={increment2}>Clicks: {count2}</button>
 
 <h3>{string}</h3>
 
