a80340294bdccdfad2e0081367a07c8cda4cc72a
Brandon Young <brandon@byitkc.com>
2023-07-11 07:47:05 -0500

    Moving practice project to dedicated folder

---
+++ udemy02/practice/src/App.svelte
@@ -0,0 +1,102 @@
+<script>
+    import ContactCard from "./ContactCard.svelte";
+
+    let name = "Max";
+    let title = "";
+    let image = "";
+    let description = "";
+    let formState = "empty";
+
+    let createdContacts = [];
+
+    function addContact() {
+        if (
+            name.trim().length == 0 ||
+            title.trim().length == 0 ||
+            description.trim().length == 0 ||
+            description.trim().length == 0
+        ) {
+            formState = "invalid";
+            return;
+        }
+        createdContacts = [
+            ...createdContacts,
+            {
+                id: Math.random(),
+                name: name,
+                jobTitle: title,
+                imageUrl: image,
+                description: description,
+            },
+        ];
+        formState = "done";
+    }
+
+    function deleteFirst() {
+        createdContacts = createdContacts.slice(1);
+    }
+
+    function deleteLast() {
+        createdContacts = createdContacts.slice(0, -1);
+    }
+</script>
+
+<form id="form">
+    <div class="form-control">
+        <label for="userName">User Name</label>
+        <input type="text" bind:value={name} id="userName" />
+    </div>
+    <div class="form-control">
+        <label for="jobTitle">Job Title</label>
+        <input type="text" bind:value={title} id="jobTitle" />
+    </div>
+    <div class="form-control">
+        <label for="image">Image URL</label>
+        <input type="text" bind:value={image} id="image" />
+    </div>
+    <div class="form-control">
+        <label for="desc">Description</label>
+        <textarea rows="3" bind:value={description} id="desc" />
+    </div>
+    <!-- Without the "preventDefault" here the page would reload the page on form submission. The "preventDefault" modifier ensures that the default action of submission doens't occur -->
+    <button on:click|preventDefault={addContact} type="submit"
+        >Add Contact Card</button
+    >
+</form>
+<!-- The "once" modifier ensures that the function is only ran one time -->
+<button on:click|once={deleteFirst}>Delete First</button>
+<button on:click={deleteLast}>Delete Last</button>
+
+{#if formState === "invalid"}
+    <p class="error">Please fill in all fields</p>
+{:else}
+    <p>Fill out the form to add a contact card</p>
+{/if}
+
+{#each createdContacts as contact, index (contact.id)}
+    <h2># {index + 1}</h2>
+    <ContactCard
+        userName={contact.name}
+        jobTitle={contact.jobTitle}
+        description={contact.description}
+        userImage={contact.imageUrl}
+    />
+{:else}
+    <p class="warning">⚠️ No contacts created yet!</p>
+{/each}
+
+<style>
+    #form {
+        width: 30rem;
+        max-width: 100%;
+        margin: 1rem 0;
+    }
+
+    .warning {
+        color: orange;
+    }
+
+    .error {
+        color: red;
+    }
+</style>
