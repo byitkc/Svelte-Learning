c61d632df928c7d0b8b794ba9605cd24679824de
Brandon Young <brandon@byitkc.com>
2023-07-13 08:10:40 -0500

    Split form into separate component

--- udemy02/project/src/App.svelte
+++ udemy02/project/src/App.svelte
@@ -1,8 +1,17 @@
 <script>
     import Header from "./UI/Header.svelte";
     import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+    import TextInput from "./UI/TextInput.svelte";
+    import Button from "./UI/Button.svelte";
 
-    const meetups = [
+    let title = "";
+    let subtitle = "";
+    let imageUrl = "";
+    let address = "";
+    let email = "";
+    let description = "";
+
+    let meetups = [
         {
             id: "m1",
             title: "Coding Bootcamp",
@@ -25,16 +34,82 @@
             contactEmail: "swim@test.com",
         },
     ];
+
+    function addMeetup() {
+        const newMeetup = {
+            id: Math.random().toString(),
+            title: title,
+            subtitle: subtitle,
+            imageUrl: imageUrl,
+            address: address,
+            contactEmail: email,
+            description: description,
+        };
+
+        // meetups.push(newMeetup); // DOES NOT WORK!
+        meetups = [newMeetup, ...meetups];
+    }
 </script>
 
 <Header />
 
 <main>
+    <form on:submit|preventDefault={addMeetup}>
+        <TextInput
+            id="title"
+            label="Title"
+            value={title}
+            type="text"
+            on:input={(event) => (title = event.target.value)}
+        />
+        <TextInput
+            id="subtitle"
+            label="Subtitle"
+            value={subtitle}
+            type="text"
+            on:input={(event) => (subtitle = event.target.value)}
+        />
+        <TextInput
+            id="address"
+            label="Address"
+            value={address}
+            type="text"
+            on:input={(event) => (address = event.target.value)}
+        />
+        <TextInput
+            id="imageUrl"
+            label="Image URL"
+            value={imageUrl}
+            type="text"
+            on:input={(event) => (imageUrl = event.target.value)}
+        />
+        <TextInput
+            id="email"
+            label="Email"
+            value={email}
+            type="email"
+            on:input={(event) => (email = event.target.value)}
+        />
+        <TextInput
+            id="description"
+            label="Description"
+            value={description}
+            controlType="textarea"
+            on:input={(event) => (description = event.target.value)}
+        />
+        <Button type="submit" caption="Save" />
+    </form>
     <MeetupGrid {meetups} />
 </main>
 
 <style>
     main {
         margin-top: 4rem;
+    }
+
+    form {
+        width: 30rem;
+        max-width: 90%;
+        margin: auto;
     }
 </style>
