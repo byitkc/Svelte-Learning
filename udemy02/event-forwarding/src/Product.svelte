22acaa301ebe4e2cbb282aa4ed52b9827bb715fe
Brandon Young <brandon@byitkc.com>
2023-07-14 07:23:24 -0500

    Basic on:click event forwarding

---
+++ udemy02/event-forwarding/src/Product.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let productTitle;
+</script>
+
+<article>
+    <h1>{productTitle}</h1>
+    <!-- If we just set the event listener "on:click" without pointing it to a function it will forward the event -->
+    <!-- to the parent component. It only forwards up one component! -->
+    <button on:click>Add to Cart</button>
+    <!-- Just using the raw "on:click" will cause problems if we have multiple items that use "on:click" -->
+    <button on:click>Delete</button>
+</article>
