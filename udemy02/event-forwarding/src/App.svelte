4079b74fa981374bceac318326ee08bf7527f8fb
Brandon Young <brandon@byitkc.com>
2023-07-15 04:54:03 -0500

    Extracting data and custome events

--- udemy02/event-forwarding/src/App.svelte
+++ udemy02/event-forwarding/src/App.svelte
@@ -1,5 +1,17 @@
 <script>
 	import Product from "./Product.svelte";
+
+	function addToCart(event) {
+		console.log(event.detail);
+	}
+
+	function deleteProduct(event) {
+		console.log(event.detail);
+	}
 </script>
 
-<Product productTitle="A Book" on:click={() => alert("Clicked!")} />
+<Product
+	productTitle="A Book"
+	on:add-to-cart={addToCart}
+	on:delete={deleteProduct}
+/>
--- udemy02/event-forwarding/src/Product.svelte
+++ udemy02/event-forwarding/src/Product.svelte
@@ -1,12 +1,26 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+
     export let productTitle;
+
+    const dispatch = createEventDispatcher();
+
+    function addToCart() {
+        dispatch("add-to-cart", {
+            id: "p1",
+        });
+    }
+
+    function deleteProduct() {
+        dispatch("delete", "p1");
+    }
 </script>
 
 <article>
     <h1>{productTitle}</h1>
     <!-- If we just set the event listener "on:click" without pointing it to a function it will forward the event -->
     <!-- to the parent component. It only forwards up one component! -->
-    <button on:click>Add to Cart</button>
+    <button on:click={addToCart}>Add to Cart</button>
     <!-- Just using the raw "on:click" will cause problems if we have multiple items that use "on:click" -->
-    <button on:click>Delete</button>
+    <button on:click={deleteProduct}>Delete</button>
 </article>
