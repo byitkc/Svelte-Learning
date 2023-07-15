01ce71a23279f776cdfbf33a4476a4f33a61f7cd
Brandon Young <brandon@byitkc.com>
2023-07-15 09:52:05 -0500

    Binding refresher

---
+++ udemy02/binding-forms/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let value = "Brandon";
+
+    $: console.log(value);
+
+    function setValue(event) {
+        value = event.target.value;
+    }
+</script>
+
+<!-- <input type="text" {value} on:input={setValue} /> -->
+<input type="text" bind:value />
