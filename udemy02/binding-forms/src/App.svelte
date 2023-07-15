18656fda444ef3548eb8f3758e419413ac8bcdb6
Brandon Young <brandon@byitkc.com>
2023-07-15 10:03:43 -0500

    Working with numbers

--- udemy02/binding-forms/src/App.svelte
+++ udemy02/binding-forms/src/App.svelte
@@ -3,10 +3,12 @@
     import Toggle from "./Toggle.svelte";
 
     let value = "Brandon";
+    let price;
     let selectedOption = 1;
 
     $: console.log(value);
     $: console.log(selectedOption);
+    $: console.log(price);
 
     function setValue(event) {
         value = event.target.value;
@@ -17,3 +19,5 @@
 <!-- <input type="text" bind:value /> -->
 <CustomInput bind:value />
 <Toggle bind:chosenOption={selectedOption} />
+
+<input type="number" bind:value={price} />
