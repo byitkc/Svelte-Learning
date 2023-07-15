eb421a7e4110c0cb8b03edfb66b6176634d4a69b
Brandon Young <brandon@byitkc.com>
2023-07-15 05:35:22 -0500

    Create and update lifecycle

--- udemy02/event-forwarding/src/Modal.svelte
+++ udemy02/event-forwarding/src/Modal.svelte
@@ -1,9 +1,40 @@
 <script>
-    import { createEventDispatcher } from "svelte";
+    import {
+        createEventDispatcher,
+        onMount,
+        onDestroy,
+        beforeUpdate,
+        afterUpdate,
+    } from "svelte";
 
     const dispatch = createEventDispatcher();
 
     let agreed = false;
+    let autoScroll = false;
+
+    onMount(() => {
+        console.log("mounted");
+    });
+
+    onDestroy(() => {
+        console.log("destroyed");
+    });
+
+    // These functions run after every update to the component
+    beforeUpdate(() => {
+        console.log("before update");
+        autoScroll = agreed;
+    });
+
+    afterUpdate(() => {
+        console.log("after update");
+        if (autoScroll) {
+            const modal = document.querySelector(".modal");
+            modal.scrollTo(0, modal.scrollHeight);
+        }
+    });
+
+    console.log("script executed");
 </script>
 
 <div class="backdrop" on:click={() => dispatch("cancel")} />
@@ -49,7 +80,7 @@
         top: 10vh;
         left: 10%;
         width: 80%;
-        max-height: 80vh;
+        max-height: 10vh;
         background: white;
         border-radius: 5px;
         z-index: 100;
